7cae32c4b0b91398404b4bcbd68cd25ca1b80b46
Ivan Pisquiy <pisquiy22@gmail.com>
2023-01-23 10:30:47 -0600

    Página de Antonio casi terminada, incluyendo solo el apartado Front-end

---
+++ proceso.js
@@ -0,0 +1,58 @@
+
+//Function that reveals the contect while the user is scrolling
+
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+
+    for(var i = 0; i < reveals.length; i++){
+
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if( revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        } else{
+            reveals[i].classList.remove('active');
+        }
+    }
+}
+
+//Process that is used for the proyect showcase
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        0:{
+            slidesPerView: 1,
+        },
+        520:{
+            slidesPerView: 2,
+        },
+        950:{
+            slidesPerView: 3,
+        },
+    },
+  });
+
+  function OpenPDF () {
+    var iframe = $("#forPostyouradd");
+    iframe.attr("src", iframe.data("src")); 
+}
